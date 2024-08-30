48759dfbbd7b5a4afe26f21cba2c64032879657d
Galboss <bryanchaconreyes@gmail.com>
2024-08-29 18:01:11 -0600

    Adding content to the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Footer.css"
-
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
+    let year = new Date();
     return (
         <>
             <footer>
-                <div className="container">
-                    <h1>Hello world</h1>
+                <div className="container-fluid">
+                    <div className="flex-container">
+                        <div>
+                            <a>
+                                <h3>
+                                    <Icon icon={faLinkedin} />&nbsp;Bryan José Chacón Reyes
+                                </h3>
+                            </a>
+                            <h5>Software Developer</h5>
+                        </div>
+                        <div>
+                            <h2>Bryan Chacón™ &nbsp;{year.getFullYear()}</h2>
+                        </div>
+                        <div>
+                            All the information belongs to Huawei Enterprise
+                        </div>
+                    </div>
                 </div>
             </footer>
         </>
